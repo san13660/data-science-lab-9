b15914ab4846feef4a7a91bc6a98bd6a031caf71
Estuardo Diaz <estuardoremi@gmail.com>
2020-11-02 00:57:48 -0600

    Mejoras Dahsboard

--- Lab9/app.R
+++ Lab9/app.R
@@ -9,45 +9,106 @@
 
 library(shiny)
 library("gdata")
+library(shinydashboard)
 
 # Read csv for
 mydata = read.csv(paste(gsub("Lab9","",getwd()),"/importacionesVehiculosSAT.csv", sep=""))
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Importacion de vehiculos"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+ui <- dashboardPage(
+    dashboardHeader(title = "Importaciones Vehiculos SAT"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Valor de Carros", tabName = "dashboard", icon = icon("dashboard")),
+            menuItem("Carros por año", tabName = "widgets1", icon = icon("th")),
+            menuItem("Valor e Impuestos", tabName = "widgets2", icon = icon("th"))
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            # First tab content
+            tabItem(tabName = "dashboard",
+                    fluidRow(
+                        box(plotOutput("plot1", height = 250)),
+                        sidebarPanel(
+                            sliderInput("bins",
+                                        "Number of bins:",
+                                        min = 1,
+                                        max = 50,
+                                        value = 30)
+                        )
+                    )
+            ),
+            
+            # Second tab content
+            tabItem(tabName = "widgets1",
+                    fluidRow(
+                        box(plotOutput("plot2", height = 250)),
+                        
+                        box(plotOutput("plot3", height = 250))
+                    )
+            ),
+            
+            # Third tab content
+            tabItem(tabName = "widgets2",
+                    fluidRow(
+                        box(plotOutput("plot4", height = 250)),
+                        
+                        box(plotOutput("plot5", height = 250))
+                    )
+            )
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- mydata["Anio"][,1]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
+    output$plot1 <- renderPlot({
+        x <- mydata["Valor.CIF"][,1]
+        x <- x[x > 100000 & x < 500000]
+        bins <- seq(100000, 500000, length.out = input$bins + 1)
+        
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        hist(x, breaks = bins, col = 'lightsteelblue', border = 'white', 
+             main = "Valor de carros", xlab = "Valor $", ylab = "Cantidad")
+    })
+    
+    output$plot2 <- renderPlot({
+        x    <- mydata["Anio"][,1]
+        bins <- seq(2011, 2019, length.out = 10)
+        
+        hist(x, breaks = bins, col = 'lightsteelblue', border = 'white',
+             main = "Cantidad de carros por año", 
+             xlab = "Año", ylab = "Cantidad")
+    })
+    
+    output$plot3 <- renderPlot({
+        x    <- mydata["Mes"][,1]
+        bins <- seq(1, 12, length.out = 12)
+        
+        hist(x, breaks = bins, col = 'lightsteelblue', border = 'white',
+             main = "Cantidad de carros al mes", 
+             xlab = "Mes", ylab = "Cantidad")
+    })
+    
+    output$plot4 <- renderPlot({
+        x <- mydata["Valor.CIF"][,1]
+        x <- x[x > 100000 & x < 500000]
+        bins <- seq(100000, 500000, length.out = 10)
+        
+        hist(x, breaks = bins, col = 'lightsteelblue', border = 'white',
+             main = "Valor CIF vs Impuesto", 
+             xlab = "Valor CIF", ylab = "Cantidad")
+    })
+    
+    output$plot5 <- renderPlot({
+        x    <- mydata["Impuesto"][,1]
+        x <- x[x > 2874 & x < 70625]
+        bins <- seq(2874, 70625, length.out = 10)
+        hist(x, breaks = bins, col = 'lightsteelblue', border = 'white',
+             main = "Impuesto pagado", 
+             xlab = "Impuesto", ylab = "Cantidad")
     })
 }
 
+shinyApp(ui, server)
 # Run the application 
 shinyApp(ui = ui, server = server)
